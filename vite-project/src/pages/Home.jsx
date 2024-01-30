b41ed8b51d126f77ce3b2b0198371b1cf16cec55
Fariz <farizbakur@gmail.com>
2024-01-30 21:43:18 +0700

    adding tailwind css

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className=' font-bold text-4xl text-blue-700 hover:text-red-700 transition-all duration-500 '>Home</div>
   )
 }
 
